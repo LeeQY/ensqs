5a5816a58a28958040ce2de953d86c6885c70095
LeeQ <leeq@me.com>
2015-12-10 20:24:34 +0800

    Change ensqs methods to jobber.

--- ensqs.go
+++ ensqs.go
@@ -1,21 +1,14 @@
 package ensqs
 
 import (
-	"errors"
 	"log"
 	"strconv"
-	"sync"
 
+	"github.com/LeeQY/jobber"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
-
-type InfoQ struct {
-	m          sync.RWMutex
-	values     []Value
-	processing bool
-}
 
 type Value struct {
 	Key *string
@@ -25,13 +18,12 @@ type Value struct {
 var (
 	qURL string
 
-	infoQ InfoQ
-	//	count int32
-
 	thisSQS   *sqs.SQS
 	waitTime  = int64(20)
 	binary    = "Binary"
 	valueName = "value"
+
+	j *jobber.Jobber
 )
 
 // Need to set once.
@@ -42,62 +34,29 @@ func SetInfo(queueURL, region *string) {
 	qURL = *queueURL
 
 	thisSQS = sqs.New(session.New(&aws.Config{Region: region}))
+
+	j = jobber.New(batchSend, 10)
 }
 
-func AddValue(v *Value) error {
-	if v == nil {
-		return errors.New("Value can't be nil.")
-	}
-	infoQ.m.Lock()
-	defer infoQ.m.Unlock()
-
-	infoQ.values = append(infoQ.values, *v)
-	if !infoQ.processing {
-		infoQ.processing = true
-		go send()
-	}
-	return nil
+func AddValue(v *Value) {
+	j.AddJob(v)
 }
 
-func getValues() []Value {
-	infoQ.m.RLock()
-	defer infoQ.m.RUnlock()
-
-	if len(infoQ.values) > 10 {
-		return infoQ.values[:10]
-	} else {
-		return infoQ.values
-	}
-}
-
-func deleteValue(l int) {
-	infoQ.m.Lock()
-	defer infoQ.m.Unlock()
-
-	infoQ.values = infoQ.values[l:]
-}
-
-func send() {
-	values := getValues()
+func batchSend(values []interface{}) bool {
 	l := len(values)
-
-	if l == 0 {
-		infoQ.m.Lock()
-		infoQ.processing = false
-		infoQ.m.Unlock()
-		return
-	}
-
 	entries := make([]*sqs.SendMessageBatchRequestEntry, l)
+
 	for i := 0; i < l; i++ {
 		s := strconv.Itoa(i)
 
+		value := values[i].(*Value)
+
 		var one sqs.SendMessageBatchRequestEntry
 		one.Id = &s
-		one.MessageBody = values[i].Key
+		one.MessageBody = (*value).Key
 
 		var mav sqs.MessageAttributeValue
-		mav.BinaryValue = values[i].V
+		mav.BinaryValue = (*value).V
 		mav.DataType = &binary
 
 		one.MessageAttributes = make(map[string]*sqs.MessageAttributeValue)
@@ -113,10 +72,7 @@ func send() {
 
 	if _, err := thisSQS.SendMessageBatch(params); err != nil {
 		log.Println(err)
-	} else {
-		deleteValue(l)
-		//	atomic.AddInt32(&count, int32(l))
+		return false
 	}
-
-	send()
+	return true
 }
